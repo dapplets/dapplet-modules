5ce0c815d3a79e6ae74eba6f2d0d8dc8639993f7
ethernian <dima@ethernian.com>
2019-05-07 01:16:12 +0200

    [+] T_ButtonConfig, T_MenuItemConfig

--- architecture-v2/strong-typing-example.ts
+++ architecture-v2/strong-typing-example.ts
@@ -12,21 +12,30 @@ namespace scratchpad_01 {
         //ToDo: any member?
     }
 
-    type T_ActionFactory = (viewId:string,insPoint:string,ctx:any)=>any;
-    type T_TwitterActionFactoryConstructor = (config:any) => T_ActionFactory;
+    type T_ActionFactory = (view:IView,insPoint:string,ctx:any)=>any;
 //#endregion COMMON INTERFACES
 
 //#region TWITTER INTERFACES
+    type T_TwitterActionFactoryConstructor<T> = (config:T) => T_ActionFactory;
     type T_TwitterAdapterConfig = {[key in keyof T_TwitterViewSet]:({[key:string]:T_ActionFactory[]})}
     type T_TwitterActionFactoryConstructorSet = {
-        button: T_TwitterActionFactoryConstructor;
-        menuItem: T_TwitterActionFactoryConstructor;
+        button: T_TwitterActionFactoryConstructor<T_ButtonConfig>;
+        menuItem: T_TwitterActionFactoryConstructor<T_MenuItemConfig>;
     }
     type T_TwitterViewSet = {
         TIMELINE_VIEW?:  IView;
         DIRECT_MESSAGE?: IView;
     }
-
+    type T_ButtonConfig = {
+        img: string;
+        exec : (ctx:any) => void;
+        label?: string;
+    }
+    type T_MenuItemConfig = {
+        text: string;
+        exec : (ctx:any) => void;
+    }
+    
     //ToDo: define content/context types
     type TweetContext = {
         id: number;
@@ -49,9 +58,19 @@ namespace scratchpad_01 {
         public constructor(public readonly core:ICore){}
 
         private readonly factories : T_TwitterActionFactoryConstructorSet = {
-            button: null,  //ToDo: implement
-            menuItem: null //ToDo: implement
+            button : ({img, exec, label} : T_ButtonConfig) => ( 
+                (view:IView, insPoint:ID, ctx:any) => this.TO_BE_IMPLEMENTED(
+                    "<div><img src='${img}'/>(${label}) </div>", ()=>exec(ctx)
+                )
+            ),
+            menuItem : ({text, exec} : T_MenuItemConfig) => ( 
+                (view:IView, insPoint:ID, ctx:any) => this.TO_BE_IMPLEMENTED(
+                    "<div><MENU_ITEM>${text}</MENU_ITEM></div>", ()=>exec(ctx)
+                )
+            )
         }
+
+        private TO_BE_IMPLEMENTED(html:string, exec: Function):void {}
 
         public main():void{
             let feature = new TwitterFeature(this.core);
