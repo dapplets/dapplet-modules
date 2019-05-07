aa1168b728a53447475297dd9822a5155304912b
Alexander Sakhaev <alsakhaev@gmail.com>
2019-05-07 10:33:20 +0300

    playing with strong typed version

--- extension/src/content_script.ts
+++ extension/src/content_script.ts
@@ -1,6 +1,52 @@
 import { initBGFunctions } from "chrome-extension-message-wrapper";
 
-import { IContentAdapter, IFeature } from './interfaces';
+import { ID, IAdapter, IFeature, IView } from './interfaces';
+
+//#region TWITTER INTERFACES
+//ToDo: define more specific return type instead of "any" 
+//ToDo: is more specific context type instead of "any" really necessary?  
+type T_TwitterActionFactory = (view: IView, insPoint: string, ctx: T_TwitterContent) => any;
+type T_TwitterActionFactoryConstructor<T> = (config: T) => T_TwitterActionFactory;
+//ToDo:  !!! [key:string] must be constrained
+type T_TwitterAdapterConfig = { [key in keyof T_TwitterViewSet]: ({ [key: string]: T_TwitterActionFactory[] }) }
+type T_TwitterActionFactoryConstructorSet = {
+    button: T_TwitterActionFactoryConstructor<T_ButtonConfig>;
+    menuItem: T_TwitterActionFactoryConstructor<T_MenuItemConfig>;
+}
+type T_TwitterViewSet = {
+    TIMELINE_VIEW?: IView;
+    DIRECT_MESSAGE?: IView;
+}
+type T_ButtonConfig = {
+    img: string;
+    //ToDo: is more specific context type instead of "any" really necessary?  
+    exec: (ctx: any) => void;
+    label?: string;
+}
+type T_MenuItemConfig = {
+    text: string;
+    //ToDo: is more specific context type instead of "any" really necessary?  
+    exec: (ctx: any) => void;
+}
+
+//ToDo: define content/context types
+//ToDo: may be graphQL-like definition?
+type T_TwitterContent = Tweet | DIRECT_MESSAGE;
+type Tweet = {
+    id: number;
+    text: string;
+}
+type DIRECT_MESSAGE = {
+    id: number;
+    text: string;
+    from_name: string;
+}
+
+interface ITwitterFeature extends IFeature {
+    getFactoryConfig({ button, menuItem }: T_TwitterActionFactoryConstructorSet): T_TwitterAdapterConfig;
+}
+
+//#endregion TWITTER INTERFACES
 
 function pause(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -14,35 +60,24 @@ async function init() {
     const featureCode: string = await bgFetchText("/features/PMFeature.js");
     const Feature = eval('(function(){ ' + featureCode + ' return Feature; })();');
 
-    const feature: IFeature = new Feature();
-    const adapterId = feature.getContentAdapterId();
+    const adapterId : ID = Feature.adapterId;
     console.log('adapterId', adapterId);
+   
 
     // ============ ADAPTER LOADING ================
 
     const adapterCode: string = await bgFetchText(`/adapters/${adapterId}.js`);
-    const ContentAdapter = eval('(function(){ ' + adapterCode + ' return ContentAdapter; })();');
+    const Adapter = eval('(function(){ ' + adapterCode + ' return Adapter; })();');
+    
+    const adapter: IAdapter = new Adapter();
 
-    const adapter: IContentAdapter = new ContentAdapter();
-
-    const isSiteCompatible = adapter.isSiteCompatible(document);
-    console.log('isSiteCompatible', isSiteCompatible);
-    if (!isSiteCompatible) return;
-
-    const isPageCompatible = adapter.isPageCompatible(document);
-    console.log('isPageCompatible', isPageCompatible);
-    if (!isPageCompatible) return;
-
-    // ============ ADAPTER ACTIVATION =============
-
-    adapter.activate(document);
-    console.log('activated');
+    const feature: IFeature = new Feature();
 
     // ============ FEATURE ATTACHING =============
 
     adapter.attachFeature(feature);
     console.log('attached');
-
+ 
     // ============ ADAPTER DEACTIVATION 5 SECONDS =============
 
     // await pause(10000);
--- extension/src/interfaces.ts
+++ extension/src/interfaces.ts
@@ -24,31 +24,63 @@
 //     getContentAdapterId(): string;
 // }
 
+// //#region COMMON INTERFACES
+// export interface IExtension {
+//     openOverlay(id:ID, ctx: any):void;
+//     sendWalletConnectTx(tx:any):void;
+// }
+
+// export interface IModule { }
+
+// export interface IContentAdapter extends IModule {
+//     init(core: IExtension, doc: Document):void;
+// }
+
+// export interface IFeature extends IModule {
+//     getAugmentationConfig(actionFactories:{[key:string]:Function}, core:IExtension): any;
+// }
+
+// export interface IAction {}
+
+// export interface IView {
+//     INSERT_POINTS : ID[];
+//     attachActions(action: IAction, insPoint:ID): void; 
+//     activate(doc:Document): void;
+//     deactivate(doc:Document): void;
+// }
+
+
+// export type ID = string;
+// //#endregion COMMON INTERFACES
+
 //#region COMMON INTERFACES
-export interface IExtension {
-    openOverlay(id:ID, ctx: any):void;
-    sendWalletConnectTx(tx:any):void;
+
+export type ID = string; // 0x1234...ef
+
+export type T_ActionFactory = (view: IView, insPoint: string, ctx: any) => any;
+
+export interface ICore {
+    openOverlay(overlayId: ID): void;
 }
 
-export interface IModule { }
-
-export interface IContentAdapter extends IModule {
-    init(core: IExtension, doc: Document):void;
+export interface IAdapter {
+    //ToDo: any member?
+    attachFeature(feature: IFeature): void;
 }
 
-export interface IFeature extends IModule {
-    getAugmentationConfig(actionFactories:{[key:string]:Function}, core:IExtension): any;
+export interface IFeature {
+    //ToDo: any member?
 }
 
-export interface IAction {}
+export interface IAction {
+
+}
 
 export interface IView {
-    INSERT_POINTS : ID[];
-    attachActions(action: IAction, insPoint:ID): void; 
-    activate(doc:Document): void;
-    deactivate(doc:Document): void;
+    INSERT_POINTS: ID[];
+    attachActions(action: IAction, insPoint: ID): void;
+    activate(doc: Document): void;
+    deactivate(doc: Document): void;
 }
 
-
-export type ID = string;
 //#endregion COMMON INTERFACES
