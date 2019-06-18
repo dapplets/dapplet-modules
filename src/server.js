4c5ce9d5d81cbd81d2f0ae5a1a77bf20092fa929
Alexander Sakhaev <alsakhaev@gmail.com>
2019-06-18 10:25:47 +0300

    build history storage

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
+const fs = require('fs');
 
-app.use(express.static('src/public', { etag: false }));
-app.use('/dist', express.static('dist', { etag: false }));
+
+//app.use(express.static('src/public', { etag: false }));
+app.use('/dist', express.static('dist', {
+    etag: false
+}));
 
 app.ws('/', function (ws, req) {
-    
+
     ws.on('message', function (msg) {
         console.log('recieved: ' + msg);
         const response = msg.length;
@@ -15,6 +19,30 @@ app.ws('/', function (ws, req) {
     });
     console.log('socket connected');
 
+});
+
+app.get('/index.json', function (req, res) {
+    const distPath = './dist/';
+    const indexPath = './src/public/index.json';
+    const scriptsPath = 'scripts';
+
+    fs.readFile(indexPath, (err, data) => {
+        let config = JSON.parse(data);
+        config[scriptsPath] = {};
+
+        const names = fs.readdirSync(distPath);
+        for (const name of names) {
+            config[scriptsPath][name] = {};
+            const files = fs.readdirSync(distPath + name + '/');
+            for (const file of files) {
+                const version = file.substr(0, file.indexOf('.js'));
+                config[scriptsPath][name][version] = `dist/${name}/${file}`;
+            }
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(config, null, 3));
+    });
 });
 
 
