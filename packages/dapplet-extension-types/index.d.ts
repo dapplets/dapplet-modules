70cb871263c21802c54e5df443b6362102e446fb
Alexander Sakhaev <alsakhaev@gmail.com>
2019-06-27 10:41:08 +0300

    removed websocket proxy

--- packages/dapplet-extension-types/index.d.ts
+++ packages/dapplet-extension-types/index.d.ts
@@ -48,15 +48,4 @@ declare global {
         abstract activate(): void;
         abstract deactivate(): void;
     }
-
-    export class WebSocketProxyClient {
-        constructor(url: string);
-        public readonly url: string;
-        public onclose: () => void;
-        public onmessage: (data: string) => void;
-        public onopen: () => void;
-        public onerror: () => void;
-        public close(): void;
-        public send(data: string): void;
-    }
 }
