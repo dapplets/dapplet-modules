31d8cb3d0eb82fc2da089496d883a2bc1b780e8b
Dmitry Palchun <dima@ethernian.com>
2019-12-15 22:27:00 +0100

    [+] StM binding: WiP

--- packages/twitter-adapter-new/src/common/widget.ts
+++ packages/twitter-adapter-new/src/common/widget.ts
@@ -1,12 +1,15 @@
 export abstract class Widget<T> {
-
-    constructor(config: any) {
+    private static idc = 0
+    private sub: Subscription
+    constructor(config: any, conn?: Connection) {
         this.state = this.createState(config);
+        this.sub = conn?.subscribe('wid.' + Widget.idc++, m => Object.assign(this.state, m))
     }
 
     abstract mount(): void;
     
     public unmount(): void {
+        this.sub?.close()        
         this.el && this.el.remove();
     }
 
@@ -14,11 +17,22 @@ export abstract class Widget<T> {
 
     public createState(state) {
         const me = this;
+        let _set = function (target, property, value) {
+            target[property] = value
+            me.mount()                                  //ToDo: forward the set call into the me.mount() 
+            return true                                 // no need to return true?
+        }
         return new Proxy(state, {
-            set(target, property, value) {
-                target[property] = value;
-                me.mount();
-                return true;
+            set(target, property, value) {   //ToDo: call once on multiple setter calls
+                if (value instanceof Promise) {
+                    value.then(value  => _set(target, property, value))
+                         .catch(value => _set(target, property, undefined)) 
+                } else if (value instanceof Subscription) {
+                    value.onMessage(value  => _set(target, property, value))
+                         .catch(value => _set(target, property, undefined))    //ToDo: dedicated value for Errors?
+                } else {     
+                    return _set(target, property, value)
+                }
             }
         });
     }
--- packages/twitter-feature-2/src/index.ts
+++ packages/twitter-feature-2/src/index.ts
@@ -2,6 +2,31 @@ import { IFeature } from '@dapplets/dapplet-extension-types'
 import { ITwitterAdapter, T_TwitterFeatureConfig } from '@dapplets/twitter-adapter/src/types'
 import ETHEREUM_ICON from './ethereum.png'
 
+// 1. Augmentation Server 
+//      - connection created with Feature
+//      - responsible for any button of the feature
+//      - needs ctxID
+//      - data transformations?
+// 2. Overlay & 3. LongTerm calls (WalletConnect)
+//      - connection created on button action
+//      - connection dedicated to specific button
+//      - needs Context
+//      - data transformations?
+
+// Security
+// 1. Augmentation Server not audited, but can change any button state? 
+// 1.a  Should we restrict the set of changeable properties in state?
+
+
+//ToDo: use symbols?
+enum BTN {
+    READY='ready', TX_RUNNING='running', ERR='error'
+}
+
+let EthTxStatus   = (m)=>({state : m.status == 'MINED' ? BTN.READY : BTN.ERR}) 
+let OverlayStatus = (m)=>({state : m.status == 'OK' ? BTN.READY : BTN.ERR}) 
+
+
 @Injectable
 export default class TwitterFeature implements IFeature {
 
@@ -13,28 +38,23 @@ export default class TwitterFeature implements IFeature {
     public orderIndex: null;
 
     constructor() {
+        //ToDo: use Generics for state conversions
+        let sendTx = (state, ctx) => Core.sendWalletConnectTx2('1', ctx).map(EthTxStatus)
+        let openOverlay = (state, ctx) => Core.overlay2('1', ctx).map(OverlayStatus) 
+
         let { button } = this.adapter.actionFactories();
+        //ToDo: set standard icons from adaper
+        let WAITING_ICON = ETHEREUM_ICON
+        let ERROR_ICON = ETHEREUM_ICON
+
         this.config = {
             TWEET_SOUTH: [
                 button({
-                    img: ETHEREUM_ICON,
-                    exec: async function (ctx) {
-                        this.state.label = 'TX';
-                        this.state.disabled = true;
-                        this.state.loading = true;
-
-                        try {
-                            await Core.sendWalletConnectTx('1', ctx);
-                        } catch (err) {
-                            console.error(err);
-                            this.state.label = "ERR";
-                        } finally {
-                            this.state.label = "NEW";
-                            this.state.disabled = false;
-                            this.state.loading = false;
-                        }
-                    },
-                    label: "NEW"
+                    state: BTN.READY,
+                    //ToDo: use state streams for labels
+                    [BTN.READY]      : { label:'new',   img: ETHEREUM_ICON,    disabled: false,  exec: sendTx       }, 
+                    [BTN.TX_RUNNING] : { label:'tx' ,   img: WAITING_ICON,     disabled: true ,  exec: undefined    }, 
+                    [BTN.ERR]        : { label:'err',   img: ERROR_ICON,       disabled: false,  exec: openOverlay  },
                 })
             ],
             TWEET_COMBO: [],
