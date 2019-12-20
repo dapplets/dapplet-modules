41672255c36c41135cb403397df512b2aa407d09
Alexander Sakhaev <alsakhaev@gmail.com>
2019-12-20 19:42:27 +0300

    new event catching

--- packages/twitter-feature-1/src/index.ts
+++ packages/twitter-feature-1/src/index.ts
@@ -29,18 +29,33 @@ export default class TwitterFeature implements IFeature {
                             overlay.subscribe('pm_attach',
                                 async ({ market, tweet }) => {
                                     setState("TX_RUNNING");
-                                    const result = await Core.sendWalletConnectTx('1', ctx);
-                                    overlay.publish('tx_created');
-                                    setState("DEFAULT");
+                                    Core.sendWalletConnectTx('1', ctx, (e) => {
+                                        if (e.type === "CREATED") {
+                                            overlay.publish('tx_created');
+                                            setState("DEFAULT");
+                                        } else if (e.type === "PAIRING") {
+                                            setState("PAIRING");
+                                        } else if (e.type === "REJECTED") {
+                                            setState("ERR");
+                                        }
+                                    });
                                 },
                                 SubscribeOptions.SINGLE_THREAD
                             );
                         }
                     },
                     "TX_RUNNING": { 
-                        label: 'tx', 
+                        label: 'Pending', 
                         loading: true, 
                         disabled: true 
+                    },
+                    "PAIRING": { 
+                        label: 'Pairing', 
+                        loading: true, 
+                        disabled: true 
+                    },
+                    "ERR": {
+                        label: 'Error'
                     }
                 }))
             ],
