63b63aeb1f81d07f0eb51349f7c7299f066e974d
Alexander Sakhaev <alsakhaev@gmail.com>
2019-12-27 16:09:41 +0300

    added project template + cli

---
+++ templates/feature/webpack.dev.js
@@ -0,0 +1,7 @@
+const merge = require("webpack-merge");
+const common = require("../../webpack.config.js");
+
+module.exports = merge(common(__dirname), {
+  // mode: "development",
+  // devtool: "inline-source-map"
+});
