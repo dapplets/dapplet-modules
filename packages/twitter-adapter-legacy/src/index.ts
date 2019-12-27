2784e87b4689e3e4b9e8ac603faf528bb4dd74a7
Alexander Sakhaev <alsakhaev@gmail.com>
2019-12-27 14:39:38 +0300

    added todos

--- packages/twitter-adapter-legacy/src/index.ts
+++ packages/twitter-adapter-legacy/src/index.ts
@@ -9,6 +9,7 @@ export default class TwitterAdapter {
     @Inject("dynamic-adapter.dapplet-base.eth")
     private adapter: IDynamicAdapter;
 
+    // ToDo: refactor it
     public widgets = {
         button: this.adapter.createWidgetFactory<IButtonState>(Button),
         picture: this.adapter.createWidgetFactory<IPictureState>(Picture)
@@ -55,14 +56,17 @@ export default class TwitterAdapter {
         })
     }];
 
+    // ToDo: refactor it
     constructor() {
         this.adapter.attachConfig(this.config);
     }
 
+    // ToDo: refactor it
     public attachFeature(feature: IFeature): void { // ToDo: automate two-way dependency handling(?)
         this.adapter.attachFeature(feature);
     }
 
+    // ToDo: refactor it
     public detachFeature(feature: IFeature): void {
         this.adapter.detachFeature(feature);
     }
