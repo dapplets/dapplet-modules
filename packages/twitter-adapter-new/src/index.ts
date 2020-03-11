75b62996a7bec6c8cbfe41da650a653dcd07e8ce
Kirill Shulga <K.Shulga@skillunion.ru>
2020-03-11 11:41:36 +0300

    identity feature first implementation.

--- packages/twitter-adapter-new/src/index.ts
+++ packages/twitter-adapter-new/src/index.ts
@@ -86,8 +86,9 @@ export default class TwitterAdapter {
             titleInfoNode.querySelector('div.css-1dbjc4n.r-15d164r.r-1g94qm0 > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l').style.margin = '0px 0px 0px 32px';
 
             return {
-                profileFullname: titleInfoNode.querySelector('a:nth-child(1) div span span')?.innerText,
-                profileUsername: titleInfoNode.querySelector('div.css-901oao.css-bfa6kz.r-1re7ezh.r-18u37iz.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-qvutc0 span')?.innerText
+                authorFullname: titleInfoNode.querySelector('div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-dnmrzs > div > span:nth-child(1) > span')?.innerText,
+                authorUsername: titleInfoNode.querySelector('div.css-901oao.css-bfa6kz.r-1re7ezh.r-18u37iz.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-qvutc0 span')?.innerText,
+                authorImg: titleInfoNode.querySelector('a > div.css-1dbjc4n.r-1adg3ll.r-1udh08x > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > img')?.getAttribute('src')
             }
         }
     },
