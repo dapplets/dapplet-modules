34dc7ab71f4c47b45101a3d689d64a73ea21846d
Alexander Sakhaev <alsakhaev@gmail.com>
2020-08-04 12:16:44 +0300

    feat: removed dependency of identity-adapter

--- packages/dapplets-org-feature-1/src/index.ts
+++ packages/dapplets-org-feature-1/src/index.ts
@@ -2,50 +2,19 @@ import { IFeature } from '@dapplets/dapplet-extension'
 import CommonAdapter from 'common-adapter.dapplet-base.eth'
 import PICTURE from './picture.png';
 
-type Msg = {
-    uuid: string;
-    type: number;
-    text: string;
-};
-
 @Injectable
 export default class Feature {
     constructor(
         @Inject("common-adapter.dapplet-base.eth")
-        viewportAdapter: any,
-
-        @Inject("identity-adapter.dapplet-base.eth")
-        identityAdapter: any
+        viewportAdapter: any
     ) {
         // ToDo: exports in CommonAdapter type is function, but in runtime it's object.
         const { button, statusLine } = viewportAdapter.exports;
-        const overlay = Core.overlay({ url: 'https://localhost:8080', title: 'Test' });
-        const activeMessageIds = [];
 
-        identityAdapter.attachConfig({
-            events: {
-                profile_changed: async (after, before) => {
-                    if (!before || !after || after.authorUsername !== before.authorUsername) {
-                        statusLine.removeMessage(activeMessageIds);
-                        if (!after) return;
-                        const messages: Msg[] = await this.getMessages(after.authorUsername);
-                        messages.forEach(m => {
-                            statusLine.addMessage({
-                                uuid: m.uuid, 
-                                text: m.text,
-                                menu: (m.type === 0) ? () => overlay.send('test', {}) : null
-                            });
-                            activeMessageIds.push(m.uuid);
-                        });
-
-                        // messages.map(m => ({ ...m, menu: map[m.type] })).forEach(m => statusLine.addMessage(m));
-
-                        // statusLine.addMessages(this.getMessages, { 
-                        //     0: overlay.send('test', {}) 
-                        // });
-                    }
-                }
-            }
+        statusLine.addMessage({
+            uuid: "0x01", 
+            text: "Testing message in status bar.",
+            menu: () => alert('Status bar action button clicked')
         });
 
         viewportAdapter.attachConfig({
@@ -55,28 +24,10 @@ export default class Feature {
                         disabled: false,
                         img: PICTURE,
                         label: 'Click me',
-                        exec: () => { }
+                        exec: () => alert('Floating button clicked')
                     }
                 })
             ]
         });
-    }
-
-    getMessages(p: string): Promise<Msg[]> {
-        if (p !== 'Ethernian') return Promise.resolve([]);
-        const msgs: Msg[] = [{
-            uuid: 'fb99ef73-a43c-41c8-b7de-a0c445729437',
-            text: 'I was banned! You can find me at https://google.com',
-            type: 0
-        }, {
-            uuid: 'fb99ef73-a43c-41c8-b7de-a0c445729438',
-            text: 'Hello, World!',
-            type: 0
-        }, {
-            uuid: 'fb99ef73-a43c-41c8-b7de-a0c445729439',
-            text: 'Message without any menu.',
-            type: 1
-        }];
-        return Promise.resolve(msgs);
     }
 }
