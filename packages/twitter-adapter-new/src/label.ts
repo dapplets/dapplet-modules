d5c64e3adb77ea5a1fcb46fab434dda329eea01d
Alexander Sakhaev <alsakhaev@gmail.com>
2020-10-01 11:30:48 +0300

    fix: hidden prop doesn't work in label widget

--- packages/twitter-adapter-new/src/label.ts
+++ packages/twitter-adapter-new/src/label.ts
@@ -20,37 +20,35 @@ export class Label implements IWidget<ILabelState> {
         if (!document.getElementById('dapplet-widget-label-styles')) this._injectStyles();
         if (!this.el) this._createElement();
         const { text, hidden } = this.state;
+        
+        this.el.style.display = (hidden) ? 'none' : null;
 
-        if (!hidden) {
-            if (this.state.img) {
-                if (!this.el.querySelector('img')) {
-                    const imgTag = document.createElement('img');
-                    imgTag.style.width = '18px';
-                    imgTag.style.height = '18px';
-                    imgTag.style.position = 'relative';
-                    imgTag.style.top = '2px';
-                    imgTag.style.marginRight = '4px';
-                    this.el.appendChild(imgTag);
-                }
-                const imgTag: HTMLImageElement = this.el.querySelector('img') as any;
-                imgTag.src = this.state.img;
-            } else {
-                this.el.querySelector('img')?.remove();
+        if (this.state.img) {
+            if (!this.el.querySelector('img')) {
+                const imgTag = document.createElement('img');
+                imgTag.style.width = '18px';
+                imgTag.style.height = '18px';
+                imgTag.style.position = 'relative';
+                imgTag.style.top = '2px';
+                imgTag.style.marginRight = '4px';
+                this.el.appendChild(imgTag);
             }
-
-            if (this.state.text) {
-                if (!this.el.querySelector('span')) {
-                    const imgTag = document.createElement('span');
-                    imgTag.innerText = text;
-                    this.el.appendChild(imgTag);
-                }
-                const imgTag: HTMLSpanElement = this.el.querySelector('span') as any;
-                imgTag.innerText = this.state.text;
-            } else {
-                this.el.querySelector('span')?.remove();
-            }
+            const imgTag: HTMLImageElement = this.el.querySelector('img') as any;
+            imgTag.src = this.state.img;
         } else {
-            this.el.remove();
+            this.el.querySelector('img')?.remove();
+        }
+
+        if (this.state.text) {
+            if (!this.el.querySelector('span')) {
+                const imgTag = document.createElement('span');
+                imgTag.innerText = text;
+                this.el.appendChild(imgTag);
+            }
+            const imgTag: HTMLSpanElement = this.el.querySelector('span') as any;
+            imgTag.innerText = this.state.text;
+        } else {
+            this.el.querySelector('span')?.remove();
         }
     }
 
