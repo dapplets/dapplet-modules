aeb45c89889b4b16774f3451e152e4a2598a03c0
Alexander Sakhaev <alsakhaev@gmail.com>
2021-02-26 13:49:40 +0300

    fix: the font of button is different

--- packages/twitter-adapter-new/src/button.ts
+++ packages/twitter-adapter-new/src/button.ts
@@ -43,7 +43,7 @@ export class Button implements IWidget<IButtonState> {
                             </svg>` : `<img height="18" src="${img}" class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi">`}
                         </div>
                         
-                        ${label?.toString() ? `<div class="css-1dbjc4n r-xoduu5 r-1udh08x">
+                        ${label?.toString() ? `<div class="css-1dbjc4n r-xoduu5 r-1udh08x" style="color: rgb(91, 112, 131); font-size: 13px; padding: 0 12px;">
                                 <span class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-1n0xq6e r-bcqeeo r-d3hbe1 r-1wgg2b2 r-axxi2z r-qvutc0">
                                     <span ${disabled ? 'style="color:#aaa;"' : ''} class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0">${label}</span>
                                 </span>
