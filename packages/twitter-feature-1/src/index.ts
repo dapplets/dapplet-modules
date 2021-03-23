236c005921305dd967c0a6d21134e13d6c2a58a2
Alexander Sakhaev <alsakhaev@gmail.com>
2021-03-23 09:25:59 +0300

    refactor: async dapplet loading

--- packages/twitter-feature-1/src/index.ts
+++ packages/twitter-feature-1/src/index.ts
@@ -11,78 +11,79 @@ const EVENTS_DEF = {
 
 @Injectable
 export default class TwitterFeature {
-    public config: T_TwitterFeatureConfig;
 
-    constructor(
-        @Inject("twitter-adapter.dapplet-base.eth")
-        public adapter: any
-    ) {
-        Core.storage.get('serverUrl').then(serverUrl => {
-            const wallet = Core.wallet();
-            const server = Core.connect<{ pm_num: string }>({ url: serverUrl });
+    @Inject("twitter-adapter.dapplet-base.eth")
+    public adapter: any;
 
-            // ToDo: exports in ITwitterAdapter type is function, but in runtime it's object.
-            const { button, badge } = this.adapter.exports;
-            this.config = {
-                POST_STARTER: [
-                    {
-                        label: 'Attach tweet to prediction market',
-                        exec: async (ctx) => {
+    async activate() {
+        const serverUrl = await Core.storage.get('serverUrl');
+        const wallet = Core.wallet();
+        const server = Core.connect<{ pm_num: string }>({ url: serverUrl });
+
+        // ToDo: exports in ITwitterAdapter type is function, but in runtime it's object.
+        const { button, badge } = this.adapter.exports;
+
+        this.adapter.attachConfig({
+            POST_STARTER: [
+                {
+                    label: 'Attach tweet to prediction market',
+                    exec: async (ctx) => {
+                        const overlayUrl = await Core.storage.get('overlayUrl');
+                        const overlay = Core.overlay({ url: overlayUrl, title: 'Gnosis' });
+                        overlay.sendAndListen('tweet_select', ctx, {
+                            'pm_attach': (op, { market, tweet }) => {
+                                wallet.then(w => w.sendAndListen('1', ctx, {
+                                    created: () => {
+                                        overlay.send('tx_created');
+                                    }
+                                }));
+                            }
+                        });
+                    }
+                }
+            ],
+            POST_SOUTH: [
+                button({
+                    initial: "DEFAULT",
+                    "DEFAULT": {
+                        label: server.pm_num,
+                        img: GNOSIS_ICON,
+                        disabled: false,
+                        exec: async (ctx, me) => {
                             const overlayUrl = await Core.storage.get('overlayUrl');
                             const overlay = Core.overlay({ url: overlayUrl, title: 'Gnosis' });
                             overlay.sendAndListen('tweet_select', ctx, {
                                 'pm_attach': (op, { market, tweet }) => {
+                                    me.state = 'PENDING';
                                     wallet.then(w => w.sendAndListen('1', ctx, {
+                                        rejected: () => me.state = 'ERR',
                                         created: () => {
+                                            me.state = 'DEFAULT';
                                             overlay.send('tx_created');
                                         }
                                     }));
                                 }
                             });
                         }
+                    },
+                    "PENDING": {
+                        label: 'Pending',
+                        loading: true,
+                        disabled: true
+                    },
+                    "ERR": {
+                        label: 'Error',
+                        img: GNOSIS_ICON,
+                        exec: (ctx, me) => me.state = 'DEFAULT'
                     }
-                ],
-                POST_SOUTH: [
-                    button({
-                        initial: "DEFAULT",
-                        "DEFAULT": {
-                            label: server.pm_num,
-                            img: GNOSIS_ICON,
-                            disabled: false,
-                            exec: async (ctx, me) => {
-                                const overlayUrl = await Core.storage.get('overlayUrl');
-                                const overlay = Core.overlay({ url: overlayUrl, title: 'Gnosis' });
-                                overlay.sendAndListen('tweet_select', ctx, {
-                                    'pm_attach': (op, { market, tweet }) => {
-                                        me.state = 'PENDING';
-                                        wallet.then(w => w.sendAndListen('1', ctx, {
-                                            rejected: () => me.state = 'ERR',
-                                            created: () => {
-                                                me.state = 'DEFAULT';
-                                                overlay.send('tx_created');
-                                            }
-                                        }));
-                                    }
-                                });
-                            }
-                        },
-                        "PENDING": {
-                            label: 'Pending',
-                            loading: true,
-                            disabled: true
-                        },
-                        "ERR": {
-                            label: 'Error',
-                            img: GNOSIS_ICON,
-                            exec: (ctx, me) => me.state = 'DEFAULT'
-                        }
-                    })
-                ],
-                POST_COMBO: [],
-                DM_SOUTH: []
-            }
-
-            this.adapter.attachConfig(this.config);
+                })
+            ],
+            POST_COMBO: [],
+            DM_SOUTH: []
         });
+    }
+
+    async deactivate() {
+        
     }
 }
