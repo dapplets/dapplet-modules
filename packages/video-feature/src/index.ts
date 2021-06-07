6731820540c9e373df0b3182a6fbf8cb1c7bd80f
Alexander Sakhaev <alsakhaev@gmail.com>
2021-06-07 15:31:06 +0300

    feat: introduce video feature

---
+++ packages/video-feature/src/index.ts
@@ -0,0 +1,24 @@
+import { IFeature } from '@dapplets/dapplet-extension'
+import VideoAdapter from 'video-adapter.dapplet-base.eth'
+
+@Injectable
+export default class VideoFeature implements IFeature {
+
+    @Inject("video-adapter.dapplet-base.eth")
+    public adapter: any
+
+    activate() {
+        const { caption } = this.adapter.exports;
+        this.adapter.attachConfig({
+            VIDEO: (ctx: any) => [
+                caption({
+                    "DEFAULT": {
+                        text: 'Media Downloader',
+                        from: 2051,
+                        to: 3620
+                    }
+                })
+            ]
+        })
+    }
+}
