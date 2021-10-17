0f9278b1b612b4f21c0bcbec6384569bcb4e0975
Alexander Sakhaev <alsakhaev@gmail.com>
2021-10-17 20:00:18 +0300

    feat: share link in video feature

--- packages/video-feature/src/index.ts
+++ packages/video-feature/src/index.ts
@@ -1,11 +1,22 @@
 import { IFeature } from '@dapplets/dapplet-extension'
 import VideoAdapter from 'video-adapter.dapplet-base.eth'
 
+type ShareLink = {
+    contextId: string;
+    time: number;
+}
+
 @Injectable
 export default class VideoFeature implements IFeature {
 
     @Inject("video-adapter.dapplet-base.eth")
-    public adapter: any
+    public adapter: any;
+
+    private sharelink: ShareLink = null;
+
+    constructor() {
+        Core.onShareLink(x => this.sharelink = x);
+    }
 
     activate() {
         const { caption } = this.adapter.exports;
@@ -16,12 +27,21 @@ export default class VideoFeature implements IFeature {
                         text: 'Media Downloader',
                         from: 2051,
                         to: 3620,
+                        init: (ctx) => {
+                            if (this.sharelink && ctx.id === this.sharelink.contextId) {
+                                const { time } = this.sharelink;
+                                ctx.setCurrentTime(time);
+                                // ToDo: scroll to ctx.element
+                            }
+                        },
                         exec: (ctx) => {
-                            console.log(ctx);
+                            const url = `https://twitter.com/ethswarm/status/${ctx.id}`;
+                            const link = Core.createShareLink(url, { contextId: ctx.id, time: ctx.currentTime });
+                            console.log(link);
                         }
                     }
                 })
             ]
-        })
+        });
     }
 }
