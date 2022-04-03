7d6c646eb8cef90b160eba4ebec5857c0a3f4546
Alexander Sakhaev <alsakhaev@gmail.com>
2022-04-04 01:05:35 +0300

    refactor(twitter-adapter): removed unused code from input widget

--- packages/twitter-adapter-new/src/input.ts
+++ packages/twitter-adapter-new/src/input.ts
@@ -55,77 +55,13 @@ export class Input implements IWidget<IInputState> {
     }
 
     private _createElement() {
-
-        const { theme } = this.state;
-
-        const styleTag: HTMLStyleElement = document.createElement('style');
-        styleTag.type = 'text/css';
         this.el = document.createElement('div');
-
-        if (this.insPointName === 'POST' || this.insPointName === 'QUOTE_POST') {
-            this.el.classList.add('css-1dbjc4n', 'r-18u37iz', 'r-1h0z5md');
-            if (this.insPointName === 'QUOTE_POST') this.el.style.marginTop = '12px';
-
-            styleTag.innerText = `
-                .dapplet-widget > div[role="button"] > div:hover > .css-1dbjc4n.r-xoduu5 > .r-sdzlij {
-                    background-color: rgba(29, 161, 242, 0.1); 
-                    transition-property: background-color, box-shadow; 
-                    transition-duration: 0.2s;
-                }
-                .dapplet-widget > div[role="button"] > div:hover {
-                    color: rgba(29, 161, 242, 1.00);
-                }`;
-
-        } else if (this.insPointName === 'PROFILE') {
-
-            this.el.classList.add(theme === "LIGHT" ? "dapplet-widget-profile-button" : "dapplet-widget-profile-button-dark");
-            styleTag.innerText = `
-                .dapplet-widget-profile-button {
-                    border: 1px solid rgb(207, 217, 222);
-                    padding: 0 10px;
-                    height: 35px;
-                    cursor: pointer;
-                    border-radius: 9999px;
-                    margin: 0 8px 12px 0;
-                    font-weight: 600;
-                    color: #000;
-                    box-sizing: border-box;
-                    font-size: 15px;
-                    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
-                    line-height: 33px;
-                }
-                
-                .dapplet-widget-profile-button:hover {
-                    background-color: rgba(15, 20, 25, 0.1);
-                }
-
-                .dapplet-widget-profile-button-dark {
-                    border: 1px solid rgb(83, 100, 113);
-                    padding: 0 10px;
-                    height: 35px;
-                    cursor: pointer;
-                    border-radius: 9999px;
-                    margin: 0 8px 12px 0;
-                    font-weight: 600;
-                    color: #fff;
-                    box-sizing: border-box;
-                    font-size: 15px;
-                    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
-                    line-height: 33px;
-                }
-                
-                .dapplet-widget-profile-button-dark:hover {
-                    background-color: rgba(239, 243, 244, 0.008);
-                }
-            `;
-        }
-
         this.el.addEventListener("click", e => {
             if (!this.state.disabled) {
                 this.state.exec?.(this.state.ctx, this.state);
             }
         });
-        document.head.appendChild(styleTag);
+        
         this.mount();
         this.state.init?.(this.state.ctx, this.state);
     }
