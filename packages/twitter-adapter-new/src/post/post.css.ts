04bced3314a2c234553f9c8096a6eae081b1184d
Alexander Sakhaev <alsakhaev@gmail.com>
2022-07-21 01:21:28 +0300

    fix: invalid font in custom tweets

--- packages/twitter-adapter-new/src/post/post.css.ts
+++ packages/twitter-adapter-new/src/post/post.css.ts
@@ -1,6 +1,50 @@
 import { css } from "lit";
 
 export const styles = css`
+    @font-face {
+        font-family: TwitterChirp;
+        src: url(https://abs.twimg.com/fonts/v2/chirp-regular-web.woff2)
+            format("woff2");
+        src: url(https://abs.twimg.com/fonts/v2/chirp-regular-web.woff)
+            format("woff");
+        font-weight: 400;
+        font-style: "normal";
+        font-display: "swap";
+    }
+
+    @font-face {
+        font-family: TwitterChirp;
+        src: url(https://abs.twimg.com/fonts/v2/chirp-medium-web.woff2)
+            format("woff2");
+        src: url(https://abs.twimg.com/fonts/v2/chirp-medium-web.woff)
+            format("woff");
+        font-weight: 500;
+        font-style: "normal";
+        font-display: "swap";
+    }
+
+    @font-face {
+        font-family: TwitterChirp;
+        src: url(https://abs.twimg.com/fonts/v2/chirp-bold-web.woff2)
+            format("woff2");
+        src: url(https://abs.twimg.com/fonts/v2/chirp-bold-web.woff)
+            format("woff");
+        font-weight: 700;
+        font-style: "normal";
+        font-display: "swap";
+    }
+
+    @font-face {
+        font-family: TwitterChirp;
+        src: url(https://abs.twimg.com/fonts/v2/chirp-heavy-web.woff2)
+            format("woff2");
+        src: url(https://abs.twimg.com/fonts/v2/chirp-heavy-web.woff)
+            format("woff");
+        font-weight: 800;
+        font-style: "normal";
+        font-display: "swap";
+    }
+
     :host {
         overflow: hidden;
     }
@@ -12,6 +56,7 @@ export const styles = css`
         transition-property: background-color, box-shadow;
         transition-duration: 0.2s;
         display: flex;
+        font-family: TwitterChirp;
     }
 
     .post:hover {
@@ -30,6 +75,5 @@ export const styles = css`
     }
 
     .text-container {
-        
     }
 `;
